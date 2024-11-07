2fca897a1ef33288740d824941865354c20914c5
usikninam <usikninam@gmail.com>
2024-11-07 21:18:32 +0100

    Finish 'Build a screen' chapter

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, View } from 'react-native';
+import ImageViewer from "@/components/ImageViewer";
+import Button from '@/components/Button';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme='primary' />
+        <Button label="Use this photo" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
