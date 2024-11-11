a5e23d77cdd6f8dbbb303d5cca64c6591bc26d0c
usikninam <usikninam@gmail.com>
2024-11-11 20:32:04 +0100

    Add emoji sticker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ import IconButton from '@/components/ IconButton';
 import EmojiPicker from '@/components/EmojiPicker';
 import EmojiList from '@/components/EmojiList';
 import { ImageSource } from 'expo-image';
+import EmojiSticker from '@/components/EmojiSticker';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -52,6 +53,7 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
